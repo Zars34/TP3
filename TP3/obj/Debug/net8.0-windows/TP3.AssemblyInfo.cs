6a21764f9ad1097735a62821673e64778c18229f
franco_marin34 <francomarin7331@gmail.com>
2024-06-06 14:26:58 -0300

    ddd

--- TP3/obj/Debug/net8.0-windows/TP3.AssemblyInfo.cs
+++ TP3/obj/Debug/net8.0-windows/TP3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b629592c1c433f361ba53c60eb1b817b304a6e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
